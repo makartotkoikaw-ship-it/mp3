45d9cd09c4964be9c8425d696927d94960ecd53c
makartotkoikaw-ship-it <makartotkoikaw@gmail.com>
2025-11-14 02:32:02 +0800

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Ambot Converter Bot...
+set BOT_TOKEN=%BOT_TOKEN%
+set ADMIN_TELEGRAM_ID=%ADMIN_TELEGRAM_ID%
+set DB_PATH=music.db
+
+call venv\Scripts\activate
+python ambot_aiosqlite.py
+pause
